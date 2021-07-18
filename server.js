ab7e5f1408f01155495d22ce57b0c0b4ffb5af2b
unknown <yenjyao@gmail.com>
2021-07-18 18:33:50 +0100

    Added messageboard

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/index.html")))
+app.get('/', (req, res) => {
+    const messages = [
+        {
+          text: "Hi there!",
+          user: "Amando",
+          added: new Date()
+        },
+        {
+          text: "Hello World!",
+          user: "Charles",
+          added: new Date()
+        }
+     ]
+    res.render('index.ejs', {title: "Mini MessageBoard", Messages: messages})
+})
 app.listen(port, () => console.log(`Server running at http://127.0.0.1:${port}/`))
